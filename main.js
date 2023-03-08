49bc970bb8c8e7a010aee8bd6d947868f9a3adda
ErenBtrk <ErenBtrk@hotmail.com>
2023-03-08 12:24:41 +0300

    new commit

--- main.js
+++
@@ -1,8 +0,0 @@
-const panoramaImage= new PANOLENS.ImagePanorama("images/VRVideo.png");
-const imageContainer=document.querySelector(".image-container");
-
-const viewer = new PANOLENS.Viewer({
-    container:imageContainer,
-});
-
-viewer.add(panoramaImage);
